f50f5682cb2c62375810f33c2a7406821097d221
John Mitchell <JDMitchell@fullsail.edu>
2014-01-12 05:05:34 -0500

    Erased previous script
    added collective notes from the video ‘Defining Variables’

--- Screencasts and Videos/001 Values And Variables/js/script.js
+++ Screencasts and Videos/001 Values And Variables/js/script.js
@@ -1,9 +1,11 @@
-			// Javascript
+			// Javascript comment		/*   Javascript Block Comment */
 
-var yearBorn = 1955;
-/*
-console.log("Hello World!");
-console.log(yearBorn);
-*/
 
-alert(yearBorn);
+//Storage container(variable) = Value
+//Declaration needed only once
+//You must declare before you define!!!
+
+var age;	//Declaration of the variable
+age = 38;	//Defining of the variable
+
+var yearOfBirth = 1970; //Declaration and Defining of the variable
