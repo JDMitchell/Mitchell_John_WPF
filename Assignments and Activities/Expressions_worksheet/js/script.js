cd1b5f705107b192469aa035318aeb657e7f4dda
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 04:38:32 -0500

    Added comment for the final line of the previous problem.
    Created initial variable with explanatory comments.
    
    Also created the correct display function setup.
    
    Script not tested.

--- Assignments and Activities/Expressions_worksheet/js/script.js
+++ Assignments and Activities/Expressions_worksheet/js/script.js
@@ -6,4 +6,16 @@
 var sparkysAge = 3;					//This variable represents Sparky's current age
 var dogYears = sparkysAge * 7;		//This variable represents Sparky's Age in dog years
 
-console.log("Sparky is " + sparkysAge + "human years old which is " + dogYears + "in dog years.");
+//The below line holds to desire display output in sentence format.
+console.log("Sparky is " + sparkysAge + " " + "human years old which is " + dogYears + " " + "in dog years.");
+
+
+
+//Slice of Pie part 1
+var slicesOfPizza = 8; 		//Slices of pizza per pie 
+var peopleAtParty = 10;		//The total number of people at the party
+var pizzaOrderd = 5;		//The number of pizzas ordered
+var slicesPerPerson:
+
+//The below line displays the desired information in the desired format.
+console.log("Each person ate" + " " + slicesPerPerson + " " + "slices of pizza at the party.");
