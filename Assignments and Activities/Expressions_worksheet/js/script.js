d7756a473c7e9a91e1c03bc008b635458c980895
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 06:34:26 -0500

    Added equation to compile the total bill over all.

--- Assignments and Activities/Expressions_worksheet/js/script.js
+++ Assignments and Activities/Expressions_worksheet/js/script.js
@@ -31,6 +31,8 @@ console.log("Sparky got" + " " + sparkysSlices + " " + "slices of pizza.");
 
 
 //Average Shopping Bill
-var shoppingTotals = [10, 12, 15, 25, 23] ;
+var shoppingTotals = [10, 12, 15, 25, 23];	 																			//An array that displays the weekly total
+var totalBill = shoppingTotals[0] + shoppingTotals[1] + shoppingTotals[2] + shoppingTotals[3] + shoppingTotals[4]		//Generates to total bill for all weeks
+
 
 console.log(shoppingTotals);
