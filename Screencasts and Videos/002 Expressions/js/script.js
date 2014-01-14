0a7b024c6dc54260c07dcfdee6519a9ee1605156
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 02:53:07 -0500

    Began following along with the Concatenating Screencast.
    Outcome did not space properly.

--- Screencasts and Videos/002 Expressions/js/script.js
+++ Screencasts and Videos/002 Expressions/js/script.js
@@ -1,18 +1,7 @@
-//Expressions - Casting
+//Expressions - Ca//Expressions - Concatenating
 
-//var stringVar = "6";
-
-
-//Casting functions are case specific.
-//var result = 7 + Number(stringVar);
-
-//console.log(result)
-
-var areaCode = 863;
-var firstPart = 675;
-var secPart = 1599;
-
-//(863) 675-1599
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
-
-console.log(phoneNo);
+var firstName = "John";
+var lastName = "Mitchell";
+var fullName = firstName + lastName;
+	
+console.log(fullName);
