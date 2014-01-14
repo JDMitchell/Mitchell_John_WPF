8848d0ca76f02c4df7a28386e4c051e6012433a4
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 03:34:13 -0500

    Block commented all previous code for viewing purposes.
    Learned and displayed how to you increment and decrement operators: ‘++’ and ‘——’

--- Screencasts and Videos/002 Expressions/js/script.js
+++ Screencasts and Videos/002 Expressions/js/script.js
@@ -1,5 +1,6 @@
 //Expressions - Assignment Operators
 
+/*
 var a = 3;
 a += 4;		//a = a + 4
 console.log(a);
@@ -14,4 +15,13 @@ console.log(a);
 
 var a = 3;
 a -= 4;		//a = a - 4
+console.log(a);
+*/
+
+var a = 3;
+a ++;		//a = a + 1 or a += 1
+console.log(a);
+
+var a = 3;
+a --;		//a = a - 1 or a -= 1
 console.log(a);
