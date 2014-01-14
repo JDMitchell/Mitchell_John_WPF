059ba3fb7bf12658c07169e797526dd341de4ea8
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 03:14:48 -0500

    commented out the old code from the beginning of the screencast.
    Setup the second example.

--- Screencasts and Videos/002 Expressions/js/script.js
+++ Screencasts and Videos/002 Expressions/js/script.js
@@ -1,7 +1,13 @@
 //Expressions - Ca//Expressions - Concatenating
 
-var firstName = "John";
+/*var firstName = "John";
 var lastName = "Mitchell";
 var fullName = firstName + " " + lastName;
 	
-console.log(fullName);
+console.log(fullName); */
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
