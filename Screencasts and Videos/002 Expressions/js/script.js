3be7d6b99102484a134a1ccae711e1652e194c83
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 03:29:55 -0500

    learned about the different assignment operators.
    Reset variable to 3 each time for constancy.
    display the ability and use of the +=, -=, *=, /=  operators

--- Screencasts and Videos/002 Expressions/js/script.js
+++ Screencasts and Videos/002 Expressions/js/script.js
@@ -1,5 +1,17 @@
 //Expressions - Assignment Operators
 
 var a = 3;
-a = a + 4;
+a += 4;		//a = a + 4
+console.log(a);
+
+var a = 3;
+a *= 4;		//a = a * 4
+console.log(a);
+
+var a = 3;
+a /= 4;		//a = a / 4
+console.log(a);
+
+var a = 3;
+a -= 4;		//a = a - 4
 console.log(a);
