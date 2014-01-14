59b8683fac9195490ae35c7788204376ebef2a09
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 04:23:59 -0500

    Added given variables. Created console output. Calculations displayed correctly. Sentence format need to be modified for output.

--- Assignments and Activities/Expressions_worksheet/js/script.js
+++ Assignments and Activities/Expressions_worksheet/js/script.js
@@ -1,3 +1,9 @@
 // John Mitchell
 // January 14, 2014
 // Expressions Worksheet
+
+//Dog Years
+var sparkysAge = 3;					//This variable represents Sparky's current age
+var dogYears = sparkysAge * 7;		//This variable represents Sparky's Age in dog years
+
+console.log("Sparky is " + sparkysAge + "human years old which is " + dogYears + "in dog years.");
