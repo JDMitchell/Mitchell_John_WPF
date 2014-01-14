5ab25d276ceaf6a2971a752e2a7419372cfac778
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 01:30:38 -0500

    Added the parentheses to correctly add things first then divide the whole.
    received correct avg.

--- Screencasts and Videos/002 Expressions/js/script.js
+++ Screencasts and Videos/002 Expressions/js/script.js
@@ -6,6 +6,6 @@ var quiz3 = 60;
 var quiz4 = 80;
 
 //the average is the sum of the items divided by the number of the items
-var average = quiz4 + quiz3 + quiz2 + quiz1/4;
+var average = (quiz4 + quiz3 + quiz2 + quiz1)/4;
 
 console.log(average);
