449acc150a28fad80e886e3f74f08e8dbd97af56
John Mitchell <JDMitchell@fullsail.edu>
2014-01-15 07:08:48 -0500

    Added a description. Explains why I chose the subject matter to code and where it came from.

--- Assignments and Activities/Expressions/Expressions_Wacky/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Wacky/js/script.js
@@ -5,5 +5,9 @@
 
 //This is the wacky JS file.
 
+/*
+This will be based on the happenings that occur everyday in the game EVE Online.
+I play this game for 8+ hours a day in game for over two years.
+This is the basic happens of extractions from the wallet in game and the market.
+*/
 
-alert("This is in my js file");
