3f08373a5eca26a88419225b05040fec358f1392
John Mitchell <JDMitchell@fullsail.edu>
2014-01-15 08:30:58 -0500

    Removed a not needed space in the alert and commas to separate each item.

--- Assignments and Activities/Expressions/Expressions_Wacky/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Wacky/js/script.js
@@ -13,8 +13,8 @@ This is the basic happens of extractions from the wallet in game and the market.
 
 
 var iskAmount = prompt("How much Isk is currently in your wallet?\nDo not worry about entering the appropriate commas.");	//This line requests the user put in the amount of isk that is in his in game wallet
-var minerals = ["Empty", "Tritanium", "Pyerite", "Isogen", "Mexallon", "megacyte", "Morphite", "Nocxium", "Zydrine"];	//A list of the building materials in EVE Online.  SET INDEX 0 TO EMPTY FOR SELECTION PURPOSES
-alert("These are the minerals available: " + " " + minerals[1] +  " " + minerals[2] + " " + minerals[3] + " " + minerals[4] + " " + minerals[5] + " " + minerals[6] + " " + minerals[7] + " " + minerals[8]);	//displays the list of minerals available
+var minerals = ["Empty", "Tritanium", "Pyerite", "Isogen", "Mexallon", "Megacyte", "Morphite", "Nocxium", "Zydrine"];	//A list of the building materials in EVE Online.  SET INDEX 0 TO EMPTY FOR SELECTION PURPOSES
+alert("These are the minerals available: " + minerals[1] +  ", " + minerals[2] + ", " + minerals[3] + ", " + minerals[4] + ", " + minerals[5] + ", " + minerals[6] + ", " + minerals[7] + ", " + minerals[8]);	//displays the list of minerals available
 
 console.log(iskAmount); //Display in console what the user entered.
 console.log(minerals);	//Displays the array of minerals
