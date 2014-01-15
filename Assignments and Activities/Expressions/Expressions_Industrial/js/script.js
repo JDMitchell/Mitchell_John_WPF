c06a7e7c8013438cc8544d8aaa45ca04bf5bfc61
John Mitchell <JDMitchell@fullsail.edu>
2014-01-14 23:28:19 -0500

    Added the requested header for each assignment.

--- Assignments and Activities/Expressions/Expressions_Industrial/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Industrial/js/script.js
@@ -1,1 +1,9 @@
+//John Mitchell
+//January 14, 2014
+//Expressions_Personal
+
+
+//This is the Industrial JS file.
+
+
 alert("This is in my js file");
--- Assignments and Activities/Expressions/Expressions_Personal/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Personal/js/script.js
@@ -1,1 +1,9 @@
+//John Mitchell
+//January 14, 2014
+//Expressions_Personal
+
+
+//This is the personal JS file.
+
+
 alert("This is in my js file");
