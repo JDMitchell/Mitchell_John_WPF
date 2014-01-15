d70845ad469dd19498daf52a92e8e693cad69167
John Mitchell <JDMitchell@fullsail.edu>
2014-01-15 01:38:00 -0500

    Created a description for this program.
    Created first prompt and variable and had them displayed in console.

--- Assignments and Activities/Expressions/Expressions_Personal/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Personal/js/script.js
@@ -4,6 +4,11 @@
 
 
 //This is the personal JS file.
+//This script will calculate how many tea bags I will need to make a week worths of tea.
+//It will request the average amount in gallons you drink a day.
+//It will also request specifics on how you make tea.
+//This only works if you make tea by the gallon.
 
+var teaBags = prompt("How many tea bags do you use to make a gallon of tea.")
 
-alert("This is in my js file");
+console.log(teaBags);
