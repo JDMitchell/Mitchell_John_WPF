ef0c465807a082dfdc00d053ba9b8037737ea567
John Mitchell <JDMitchell@fullsail.edu>
2014-01-15 03:14:39 -0500

    Added a necessary space for sentence formatting. Modified console outputs to display collected information into a sentence that will explain what each variable is and used for.

--- Assignments and Activities/Expressions/Expressions_Personal/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Personal/js/script.js
@@ -14,7 +14,7 @@ ALL CALCULATIONS ARE BASED UPON MY WEEKLY AND DAILY CONSUMPTION OF TEA
 */
 
 alert("It takes a gallon of water to make a gallon of tea.");	//Informs the user how much water is needed to make a gallon of tea.
-var gallonsPerDay = prompt("About how many gallons of Tea do you drink a day?\nPlease use 0.5 for half a gallon and 0.25 for a quarter of a gallon."); 	//This prompt requests the amount of tea consumed in gallons with decimal format
+var gallonsPerDay = prompt("About how many gallons of Tea do you drink a day?\nPlease use 0.50 for half a gallon and 0.25 for a quarter of a gallon."); 	//This prompt requests the amount of tea consumed in gallons with decimal format
 var amountOfTeaBags = prompt("How many tea bags do you use to make a gallon of tea?");	//This prompt requests how many tea bags the user needs to make a gallon.
 var teaBagType = prompt("What type of tea bags do you use?");	//This requests the user to manually enter the type of tea bages he/she uses.
 var cupsOfSugar = prompt("How many cups of sugar do you use to make your gallon of tea?\nUse 0.50 for half a cup and 0.25 for quarter cup.");	//Collects the amount of sugar used to make each gallon of tea.
@@ -22,7 +22,7 @@ var desiredGallonsPerWeek = prompt("How many gallons of tea would you like to ha
 
 
 console.log("You drink about " + gallonsPerDay + " gallons per day.");			//Prints collected information from prompt 1 to the console
-console.log("You use " + amountOfTeaBags + "tea bags to make a gallon of tea.");				//Prints collected information from prompt 2 to the console
-console.log(teaBagType);			//Prints collected information from prompt 3 to the console
-console.log(cupsOfSugar);			//Prints collected information from prompt 4 to the console
-console.log(desiredGallonsPerWeek);	//Prints collected information from prompt 5 to the console
+console.log("You use " + amountOfTeaBags + " tea bags to make a gallon of tea.");				//Prints collected information from prompt 2 to the console
+console.log("You use " + teaBagType + " tea bags to make your tea.");			//Prints collected information from prompt 3 to the console
+console.log("It takes you " + cupsOfSugar + " cups of sugar to make a gallon of tea.");			//Prints collected information from prompt 4 to the console
+console.log("You would like to have " + desiredGallonsPerWeek + " gallons of tea made a week.");	//Prints collected information from prompt 5 to the console
