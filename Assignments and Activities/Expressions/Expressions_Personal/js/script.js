df18d1d8856953170cc60b356f911466b68e823f
John Mitchell <JDMitchell@fullsail.edu>
2014-01-15 02:25:21 -0500

    Corrected grammatically end points for prompts in place. Added teaBagType variable All collected information printed to console.

--- Assignments and Activities/Expressions/Expressions_Personal/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Personal/js/script.js
@@ -9,8 +9,10 @@
 //It will also request specifics on how you make tea.
 //This only works if you make tea by the gallon.
 
-var gallonsPerWeek = prompt("About how many gallons of Tea do you drink a week.\nPlease use 0.5 for half a gallon and 0.25");
-var teaBags = prompt("How many tea bags do you use to make a gallon of tea.");
+var gallonsPerWeek = prompt("About how many gallons of Tea do you drink a week?\nPlease use 0.5 for half a gallon and 0.25 for a quarter of a gallon."); 	//This prompt requests the amount of tea consumed in gallons with decimal format
+var teaBags = prompt("How many tea bags do you use to make a gallon of tea?");	//This prompt requests how many tea bags the user needs to make a gallon.
+var teaBagType = prompt("What type of tea bags do you use?");	//This requests the user to manually enter the type of tea bages he/she uses.
 
-console.log(gallonsPerWeek);
-console.log(teaBags);
+console.log(gallonsPerWeek);	//Prints collected information from prompt 1 to the console
+console.log(teaBags);			//Prints collected information from prompt 2 to the console
+console.log(teaBagType);		//Prints collected information from prompt 3 to the console
