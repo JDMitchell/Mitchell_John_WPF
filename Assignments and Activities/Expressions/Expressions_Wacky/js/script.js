e06a3d73e2052c5fcacc02f55b967bf3aba7b401
John Mitchell <JDMitchell@fullsail.edu>
2014-01-15 08:02:51 -0500

    Added semicolon. Added an array of the minerals. Set array to display in the console.

--- Assignments and Activities/Expressions/Expressions_Wacky/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Wacky/js/script.js
@@ -12,7 +12,8 @@ This is the basic happens of extractions from the wallet in game and the market.
 */
 
 
-var iskAmount = prompt("How much Isk is currently in your wallet?\nDo not worry about entering the appropriate commas.")	//This line requests the user put in the amount of isk that is in his in game wallet
-
+var iskAmount = prompt("How much Isk is currently in your wallet?\nDo not worry about entering the appropriate commas.");	//This line requests the user put in the amount of isk that is in his in game wallet
+var minerals = ["Tritanium", "Pyerite", "Isogen", "Mexallon", "megacyte", "Morphite", "Nocxium", "Zydrine"];	//A list of the building materials in EVE Online.
 
 console.log(iskAmount); //Display in console what the user entered.
+console.log(minerals);	//Displays the array of minerals
