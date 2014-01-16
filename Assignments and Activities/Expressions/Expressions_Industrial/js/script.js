96ddf8bff82154ff4132974cbe318351d569ac93
John Mitchell <JDMitchell@fullsail.edu>
2014-01-15 23:34:47 -0500

    Added space for later variables.
    added prompt to gather the total number of classes in the program

--- Assignments and Activities/Expressions/Expressions_Industrial/js/script.js
+++ Assignments and Activities/Expressions/Expressions_Industrial/js/script.js
@@ -10,12 +10,21 @@ This will request the users information on the basics of their educational caree
 
 alert("The following prompts will request your information about the school you are currently attending.");  // Lets the user know what to expect from the next few lines of code.
 
+
+
+
+
+
+
+
 var schoolName = prompt("What is the name of the school you are currently attending?");	//Retrieves user input on the school name.
 console.log(schoolName);	//Displays user input in the console.
 
 var programName = prompt("What program are you enrolled in?")	//Requests the user to enter the name of their program
 console.log(programName);	//Displays user input to console.
 
+var programClasses = prompt("How many classes are there in the program?\nPlease included transfer credits and completed classes.")	//Acquires the total classes the program has
+console.log = (programClasses)	//Displays input to console.
 
 
 
