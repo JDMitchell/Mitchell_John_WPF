8d494cc9f5e8df245e3d683ff1cb1c53dd757f4c
John Mitchell <JDMitchell@fullsail.edu>
2014-01-21 18:55:40 -0500

    commented a line. Corrected a line that gave off an error.

--- Assignments and Activities/Conditionals_worksheet/js/script.js
+++ Assignments and Activities/Conditionals_worksheet/js/script.js
@@ -19,10 +19,10 @@ var degrees = prompt("Please enter the number of degrees you need covertered: \n
 var unit = prompt("Please enter the unit of temperature you would like to covert to. C/F")
 var convertedDegrees;
 if(unit === "C" || unit === "c"){
-	convertedDegrees = 5/9(degrees-32)
+	convertedDegrees = 5/9*(Number(degrees)-32)
 	console.log("The temperature is " + convertedDegrees + " degrees Celcius.")
 }else if(unit === "F" || unit === "f"){
-	convertedDegrees = 9/5*degrees+32
+	convertedDegrees = 9/5*degrees+32 //has decimal precision not rounded
 	console.log("the temperature is " + convertedDegrees + " degrees Fahrenheit.")
 }else{
 	console.log("Please choose a 'f' or 'c.'")
