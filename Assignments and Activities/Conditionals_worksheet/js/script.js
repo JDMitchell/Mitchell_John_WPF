59b6857b886142f322c531924e7c5ed22e538af6
John Mitchell <JDMitchell@fullsail.edu>
2014-01-21 18:40:24 -0500

    Added basic format of temperature coverer

--- Assignments and Activities/Conditionals_worksheet/js/script.js
+++ Assignments and Activities/Conditionals_worksheet/js/script.js
@@ -12,3 +12,15 @@ if(competitorWeight >= 250){
 }else{
 	console.log("The competitor needs to gain some weight!")
 }
+
+
+//Celcius to Fahrenheit converter
+var degrees = 32;
+var unit;
+if(unit === "F" || unit === "f"){
+	console.log("The temperature is " + + " degrees Celcius.")
+}else if(unit === "C" || unit === "c"){
+	console.log("the temperature is " + + " degrees Fahrenheit.")
+}else{
+	console.log("Please choose a 'f' or 'c.'")
+}
