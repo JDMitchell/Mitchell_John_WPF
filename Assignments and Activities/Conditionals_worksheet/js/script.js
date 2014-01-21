419e588b2f06a0f849d486f511bb081f1e901d15
John Mitchell <JDMitchell@fullsail.edu>
2014-01-21 17:40:27 -0500

    Creates if else statement that works should the users weight meat the criteria for heavyweight division

--- Assignments and Activities/Conditionals_worksheet/js/script.js
+++ Assignments and Activities/Conditionals_worksheet/js/script.js
@@ -1,1 +1,14 @@
-alert("This is in my js file");
+/*
+John Mitchell
+Expressional Worksheet
+01-21-2014
+*/
+
+
+//Stuff your Face I
+var competitorWeight = 250;
+if(competitorWeight >= 250){
+	console.log("The competitor qualifies for the heavyweight division.")
+}else{
+	console.log("The competitor needs to gain some weight!")
+}
