2639281f86ced9a067b38733b7f299c64e36bd3f
John Mitchell <JDMitchell@fullsail.edu>
2014-01-21 07:32:55 -0500

    Finished the second video - Logic Concepts

--- Screencasts and Videos/004 Conditionals/js/script.js
+++ Screencasts and Videos/004 Conditionals/js/script.js
@@ -12,3 +12,12 @@ else{
 	goToTheMovies();
 }
 */
+
+//Video - Logive Concepts
+/*
+var temp = 65;
+// Condition Operators are >(Greater Than), <(Less Than), >=(Greater Than or Equal To), <=(Less Than or Equal To), !=(Not Equal To), ==(Equal To)
+if(temp == 75){		
+	go goToTheBeach();
+}
+*/
