c82dabda9a59ee8bf0b3886fca0f52af470660af
John Mitchell <JDMitchell@fullsail.edu>
2014-01-21 08:26:10 -0500

    added variable to check conditionals comparing variable values. Tested the same.

--- Screencasts and Videos/004 Conditionals/js/script.js
+++ Screencasts and Videos/004 Conditionals/js/script.js
@@ -52,9 +52,10 @@ console.log("What comes after");
 
 //Video - Relational Expressions
 var kidHeight = 50;
+var minghieght = 48
 
 //if the kid is over 48 inches in height, print to the console "You can Ride!"
-if(kidHeight > 48){
+if(kidHeight > minghieght){
 	//Code performed if condition is true
 	console.log("You can Ride the coaster!");
 }
