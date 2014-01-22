cf41e9b914501c56f646e712ee2820ca638dcef3
John Mitchell <JDMitchell@fullsail.edu>
2014-01-21 19:21:00 -0500

    Created initial if statements added commented grading chart

--- Assignments and Activities/Conditionals_worksheet/js/script.js
+++ Assignments and Activities/Conditionals_worksheet/js/script.js
@@ -27,3 +27,51 @@ if(unit === "C" || unit === "c"){
 }else{
 	console.log("Please choose a 'f' or 'c.'")
 }
+
+//Grade calculator
+var grade = prompt("Please enter your grade percentage.\n32% would be 32.")
+var letter
+console.log(grade)
+if(grade >= 95){
+	letter = "A+"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+if(grade >= 90 && grade <= 94){
+	letter = "A"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+if(grade >= 85 && grade <= 89){
+	letter = "B+"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+if(grade >= 80 && grade <= 84){
+	letter = "B"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+if(grade >= 76 && grade <= 79){
+	letter = "C+"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+if(grade >= 73 && grade <= 75){
+	letter = "C"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+if(grade >= 70 && grade <= 72){
+	letter = "D"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+if(grade <= 69){
+	letter = "F"
+	console.log("You have a " + grade + "%, which means you have earned a(n) " + letter + " in the class!")
+}
+
+/*
+A+ 	95-100	4.0
+A 	90-94	3.5
+B+ 	85-89	3.0
+B 	80-84	2.5
+C+ 	76-79	2.0
+C 	73-75	1.5
+D 	70-72	1.0
+F 	0-69 	0.0
+*/
