904938e163dc65e434772f62d1aad44985d97f76
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 13:29:01 -0500

    Changed variable name added gas budget added comments.

--- Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
@@ -6,7 +6,10 @@ January 23, 2014
 
 
 alert("This will allow you to see if you have a car has your needed MPG rating for your trip.");	//Alerts the user of the purpose of this program
-var cars = prompt("Do you have more than one car?\n'Y/N'")	//Asks the user
+var car = prompt("Which vehicle will you be taking on this trip?")	//Asks car they will be taking.
+var budgetForGas = prompt("How much do you have available for expindetures in gas?\nYou can use exact demical if you would like.")	//Asks the user to input thier gas budget.
+
+
 
 /*if(cars === ){
 
@@ -20,4 +23,7 @@ var cars = prompt("Do you have more than one car?\n'Y/N'")	//Asks the user
 var mpgNeeded = 30
 var car = (mpgNeeded >= 32) ? "Kia" : "Car of your choice.";
 */
-console.log(cars);
+
+//Console output to make sure variables are storing correctly
+console.log(car);
+console.log(budgetForGas);
