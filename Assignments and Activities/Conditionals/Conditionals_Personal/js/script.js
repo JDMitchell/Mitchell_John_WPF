2bff2364cca9ab724c70908717b555a05b941524
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 13:18:44 -0500

    Added an alert commented out theoretical new code for later. testing first prompt

--- Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
@@ -4,17 +4,20 @@ Conditionals - Personal
 January 23, 2014
 */
 
-/*
-var car
 
-if(){
+alert("This will allow you to see if you have a car has your needed MPG rating for your trip.");	//Alerts the user of the purpose of this program
+var cars = prompt("Do you have more than one car?\n'Y/N'")	//Asks the user
 
-}else if(chosenCar === ""){
-	alert("No information entered was blank.\nPlease enter the information requesed.")	//Lets the user know there was no information entered.
+/*if(cars === ){
+
+	}else if(chosenCar === ""){
+		alert("No information entered was blank.\nPlease enter the information requesed.")	//Lets the user know there was no information entered.
+
 }else{
 
 }
-*/
+
 var mpgNeeded = 30
 var car = (mpgNeeded >= 32) ? "Kia" : "Car of your choice.";
-console.log(car);
+*/
+console.log(cars);
