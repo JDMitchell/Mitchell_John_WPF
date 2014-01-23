3aeabd89d5db7bf4b535b70e504d932e5682c8ac
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 16:40:45 -0500

    added spaces for the array added a prompt to choose and index added appropriate spacing in the prompt for readability

--- Assignments and Activities/Conditionals/Conditionals_Wacky/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Wacky/js/script.js
@@ -4,14 +4,16 @@ Conditionals - Wacky
 January 23, 2014
 */
 
-//
-var systems = ["Empty", "Playstation", "PS2", "PS3", "NES", "SNES", "N64", "Gamecube", "Wii", "Sega", "Sega Dreamcast", "Gameboy", "GBA", "DS", "3DS",] //List of available systems
-var chosenSystem;
+//displays random info about the system
+var systems = ["No System", " Playstation", " PS2", " PS3", " NES", " SNES", " N64", " Gamecube", " Wii", " Sega", " Sega Dreamcast", " Gameboy", " GBA ", " DS", " 3DS", " PSP"] //List of available systems
+//index = 0-15
+var chosenSystem = prompt(systems + "\n\nPlease choose one of the above game systems.\nNo System(0)-PSP(15)");	//requests user to make a choice
 
 
-console.log(systems) //displays the array
+console.log(chosenSystem) //displays the array
 
 /*
 else if(chosenSystem === ""){
 	alert("No information entered was blank.\nPlease enter the information requesed.")	//Lets the user know there was no information entered.
 }
+*/
