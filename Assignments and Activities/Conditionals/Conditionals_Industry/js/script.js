48b837bac6dacbfcc6e44b10a8044496527f1871
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 15:56:39 -0500

    added description edited and operator to or operator added 3 more possibilities of advice depending on users input to prompts edited else statement.

--- Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
@@ -5,6 +5,9 @@ January 23, 2014
 */
 
 
+
+//This is based on the decision not only people in the field of work I am in but people who work in general need to ask themselve when looking for work.
+
 var seekingJob = prompt("Are you looking for a job? (Y/N)")	//Asks the user if they are looking for a job.
 
 
@@ -19,12 +22,24 @@ if(seekingJob === 'Y' || seekingJob === 'y'){	//Or operator makes sure case size
 	var workFromHome = prompt("Would you like to work from home? (Y/N)")	//Asks the user if he would like to work from home.
 	console.log(workFromHome)
 
-	if((move === 'Y' || move === 'y') && (workFromHome === 'N' || workFromHome === 'n')){	//double setted logical condition
+	if((move === 'Y' || move === 'y') || (workFromHome === 'N' || workFromHome === 'n')){	//double setted logical condition
 	
-		console.log("Apply to every job you have interest in.")
+		console.log("Apply to every job you have interest in.")	//offers advice
 	
+	}else if((move === 'N' || move === 'n') || (workFromHome === 'Y' || workFromHome === 'y')){	//|| operator allows one option to be false.
+
+		console.log("Apply to every job you have interest in but tactfully deny any offers that will not allow you to work from home.")	//offers advice
+
+	}else if((move === 'N' || move === 'n') && (workFromHome === 'N' || workFromHome === 'n')){	//&& operator makes sure both conditions are true
+
+		console.log("Only apply to local jobs or jobs within a distance you are willing to travel.")	//offers advice
+
+	}else if((move === 'Y' || move === 'y') && (workFromHome === 'Y' || workFromHome === 'y')){
+
+		console.log("Apply to every job you have interest in")	//offers advice
+
 	}else{
-		console.log("something")
+		console.log("please enter 'y' for yes and 'n' for no.")
 	}
 
 
