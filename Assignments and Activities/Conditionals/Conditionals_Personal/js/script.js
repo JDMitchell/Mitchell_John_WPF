df2a1b22a4b7da1bce54b19ec5ff87de05e00335
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 13:56:18 -0500

    added prompts for new necessary elements removed unnecessary if statements for program.

--- Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
@@ -8,22 +8,18 @@ January 23, 2014
 alert("This will allow you to see if you have a car has your needed MPG rating for your trip.");	//Alerts the user of the purpose of this program
 var car = prompt("Which vehicle will you be taking on this trip?")	//Asks car they will be taking.
 var budgetForGas = prompt("How much do you have available for expindetures in gas?\nYou can use exact demical if you would like.")	//Asks the user to input thier gas budget.
+var mpg = prompt("What is your rated Mile Per Gallon(mpg)?")	//Asks user for the MPG of the vehicle being taken
+var milesToDestination = prompt("How many miles is it to your destination?\n(Please use whole numbers.)")	//Request user to enter the destination in miles being travel
+var avgGasPrice = prompt("Please enter the average price of gas expected for your trip.\nExample: 3.79")	//REquest user to enter the expected avg price of gas.
 
 
 
-/*if(cars === ){
+var  = (mpgNeeded >= 32) ? "You can make your trip!" : "You need more money for gas!";
 
-	}else if(chosenCar === ""){
-		alert("No information entered was blank.\nPlease enter the information requesed.")	//Lets the user know there was no information entered.
-
-}else{
-
-}
-
-var mpgNeeded = 30
-var car = (mpgNeeded >= 32) ? "Kia" : "Car of your choice.";
-*/
 
 //Console output to make sure variables are storing correctly
 console.log(car);
 console.log(budgetForGas);
+console.log(mpg);
+console.log(milesToDestination);
+console.log(avgGasPrice);
