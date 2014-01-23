233d41221671ee35c56aa93ee0eb7346a9feed72
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 15:29:06 -0500

    corrected the undefined that was displaying. All prompts are working correctly.

--- Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
@@ -11,16 +11,17 @@ var workFromHome = prompt("Would you like to work from home? (Y/N)")	//Asks the 
 
 //Console tested variables
 console.log(seekingJob)
-console.log(move)
 console.log(workFromHome)
 
 if(seekingJob === 'Y' || seekingJob === 'y'){	//Or operator makes sure case size does not error the user out.
 
 	var move = prompt("Are you willing to move for your new potential job? (Y/N)")	//Asks the user if he is willing to move for his new job.
+	console.log(move)
+
 
 }else if(seekingJob === 'N' || seekingJob === 'n'){	//Or operator makes sure case size does not error the user out.
 
-
+	console.log("This advisory program is not needed for you.")	//Tells the user they can discard this program
 
 }else{
 
