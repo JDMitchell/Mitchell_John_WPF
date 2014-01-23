90479a0e2fd6b73402e4809fb5e38da607d4eb68
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 15:44:16 -0500

    removed secondary prompt. added a dual condition condition statement.

--- Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
@@ -6,17 +6,28 @@ January 23, 2014
 
 
 var seekingJob = prompt("Are you looking for a job? (Y/N)")	//Asks the user if they are looking for a job.
-var workFromHome = prompt("Would you like to work from home? (Y/N)")	//Asks the user if he would like to work from home.
 
 
 //Console tested variables
 console.log(seekingJob)
-console.log(workFromHome)
 
 if(seekingJob === 'Y' || seekingJob === 'y'){	//Or operator makes sure case size does not error the user out.
 
 	var move = prompt("Are you willing to move for your new potential job? (Y/N)")	//Asks the user if he is willing to move for his new job.
 	console.log(move)
+
+	var workFromHome = prompt("Would you like to work from home? (Y/N)")	//Asks the user if he would like to work from home.
+	console.log(workFromHome)
+
+	if((move === 'Y' || move === 'y') && (workFromHome === 'N' || workFromHome === 'n')){	//double setted logical condition
+	
+		console.log("Apply to every job you have interest in.")
+	
+	}else{
+		console.log("something")
+	}
+
+
 
 
 }else if(seekingJob === 'N' || seekingJob === 'n'){	//Or operator makes sure case size does not error the user out.
