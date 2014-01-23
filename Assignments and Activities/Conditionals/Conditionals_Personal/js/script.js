c9d88cfaa26402848b571cb8d1503a594193a360
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 12:50:32 -0500

    Testing how to setup a ternary. Ternary setup correct.

--- Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Personal/js/script.js
@@ -1,1 +1,20 @@
-alert("This is in my js file");
+/*
+John Mitchell
+Conditionals - Personal
+January 23, 2014
+*/
+
+/*
+var car
+
+if(){
+
+}else if(chosenCar === ""){
+	alert("No information entered was blank.\nPlease enter the information requesed.")	//Lets the user know there was no information entered.
+}else{
+
+}
+*/
+var mpgNeeded = 30
+var car = (mpgNeeded >= 32) ? "Kia" : "Car of your choice.";
+console.log(car);
