4fac3261765ff9459db98a25214e4590ac3cdb7e
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 16:28:30 -0500

    created array. displayed to console created new variable.

--- Assignments and Activities/Conditionals/Conditionals_Wacky/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Wacky/js/script.js
@@ -1,1 +1,17 @@
-alert("This is in my js file");
+/*
+John Mitchell
+Conditionals - Wacky
+January 23, 2014
+*/
+
+//
+var systems = ["Empty", "Playstation", "PS2", "PS3", "NES", "SNES", "N64", "Gamecube", "Wii", "Sega", "Sega Dreamcast", "Gameboy", "GBA", "DS", "3DS",] //List of available systems
+var chosenSystem;
+
+
+console.log(systems) //displays the array
+
+/*
+else if(chosenSystem === ""){
+	alert("No information entered was blank.\nPlease enter the information requesed.")	//Lets the user know there was no information entered.
+}
