d331fbc33e7d794c840a7d16ea70a7c49dcc11bb
John Mitchell <JDMitchell@fullsail.edu>
2014-01-23 15:23:13 -0500

    Added correct header added initial variables to hold decision variables added comments added if-else if-else statement.

--- Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
+++ Assignments and Activities/Conditionals/Conditionals_Industry/js/script.js
@@ -1,1 +1,30 @@
-alert("This is in my js file");
+/*
+John Mitchell
+Conditionals - Industry
+January 23, 2014
+*/
+
+
+var seekingJob = prompt("Are you looking for a job? (Y/N)")	//Asks the user if they are looking for a job.
+var workFromHome = prompt("Would you like to work from home? (Y/N)")	//Asks the user if he would like to work from home.
+
+
+//Console tested variables
+console.log(seekingJob)
+console.log(move)
+console.log(workFromHome)
+
+if(seekingJob === 'Y' || seekingJob === 'y'){	//Or operator makes sure case size does not error the user out.
+
+	var move = prompt("Are you willing to move for your new potential job? (Y/N)")	//Asks the user if he is willing to move for his new job.
+
+}else if(seekingJob === 'N' || seekingJob === 'n'){	//Or operator makes sure case size does not error the user out.
+
+
+
+}else{
+
+	console.log("please enter 'y' for yes and 'n' for no.")
+
+}
+
