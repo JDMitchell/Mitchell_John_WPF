d876d036447119d70962a45e26b57a683351578e
John Mitchell <JDMitchell@fullsail.edu>
2014-01-26 17:27:58 -0500

    Completed simple circumference function

--- Assignments and Activities/Functions_worksheet/js/script.js
+++ Assignments and Activities/Functions_worksheet/js/script.js
@@ -1,1 +1,18 @@
-alert("This is in my js file");
+/*
+John Mitchell
+Functions Worksheet
+January 26, 2014
+*/
+
+//////Circumference
+
+var radius = prompt("what is the radius of your circle?")	//gathers radius from user
+
+var circumference = circumference(radius)	//creates variable to call from function
+
+function circumference(radius){	//Function
+	circumference = 2*3.14*radius	//calculates the circumference
+	return circumference;	//returns it to global area
+}
+console.log(circumference)	//prints to console
+
