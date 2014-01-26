1051ad6592f5cca06cb9c5cada37324798c9c3f1
John Mitchell <JDMitchell@fullsail.edu>
2014-01-26 15:32:53 -0500

    index.html - Corrected header and created file script.js - Created file and followed along thus far with screencasts

---
+++ Screencasts and Videos/005 Functions/js/script.js
@@ -0,0 +1,29 @@
+//Video - Introduction to Functions
+
+/*
+Video review
+*/
+
+//Video - Different Types of Functions
+/*
+explained what type of functions we will deal with
+*/
+
+//Video - Basic Funtion Structure
+
+/* - function structure
+function functionName(){
+	//code to be run
+}
+*/
+
+function outputMsg(){
+	console.log("hello Word")
+}
+
+function calcArea(){
+	var width = 20
+	var height = 30
+	var area = width * height
+	console.log(area)
+}
