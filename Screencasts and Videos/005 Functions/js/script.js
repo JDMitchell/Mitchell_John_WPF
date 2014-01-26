e700f8cd087328b2ff17186d03a6fffd2f161542
John Mitchell <JDMitchell@fullsail.edu>
2014-01-26 15:52:06 -0500

    created a function and called it several times with different input information

--- Screencasts and Videos/005 Functions/js/script.js
+++ Screencasts and Videos/005 Functions/js/script.js
@@ -53,8 +53,17 @@ Learned the difference between gloabl and loca variables
 */
 
 
+//////Video - Arguments and Parameters
 
 
+function dogYears(age){
+	
+	var dogYears = age * 7
+	console.log("Sparky is " + dogYears + " years old.")
+}
+dogYears(4);
+dogYears(5);
+dogYears(6);
 
 
 
