d4d622f2aa80d9654becbb186cc7d847865479e5
John Mitchell <JDMitchell@fullsail.edu>
2014-01-27 18:58:06 -0500

    Added initial prompts that are required for this program

--- Assignments and Activities/Functions/Industry/js/script.js
+++ Assignments and Activities/Functions/Industry/js/script.js
@@ -5,3 +5,6 @@ January 27, 2014
 */
 
 
+var computersForTheWeek = prompt("How many computers do you need to work on for the week to meet scedule?")	//Gathers quota for the week
+var daysWorking = prompt("How many days are you working this week?")	//Request how many days he is scheduled this week
+var lastDay = prompt("Is today your last day of work for the week?(Y/N)")	//Checks to see if today is his last day of the work week
