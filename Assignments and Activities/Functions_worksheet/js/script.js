2ef4c9e9f592d70f64d4d368d9e17e81711875fa
John Mitchell <JDMitchell@fullsail.edu>
2014-01-27 14:48:21 -0500

    added the Stung portion of the worksheet assignment

--- Assignments and Activities/Functions_worksheet/js/script.js
+++ Assignments and Activities/Functions_worksheet/js/script.js
@@ -14,5 +14,20 @@ function circumference(radius){	//Function
 	circumference = 2*3.14*radius	//calculates the circumference
 	return circumference;	//returns it to global area
 }
-console.log(circumference)	//prints to console
+console.log("The circumference of the circle is " +circumference)	//prints to console
+
+
+//////Stung!
+
+var victimWeight = prompt("How much do you weigh?\nPlease put in pounds:")	//gathers the victims weight
+var stingsToKill = deathStingTotal(stingsToKill)	//sets deathsting total to a global variable
+function deathStingTotal(victimWeight){		//sends parameter to function
+	var stingsPerPound = 8.666666667;	//constant variable
+	stingsToKill = victimWeight*stingsPerPound	//calculates how many stings it takes to kill
+	stingsToKill = stingsToKill.toFixed(2)
+	return stingsToKill;
+}
+
+deathStingTotal(victimWeight); //calls function
+console.log("It takes " + stingsToKill + " bee stings to kill this animal.");	// prints to the console.
 
