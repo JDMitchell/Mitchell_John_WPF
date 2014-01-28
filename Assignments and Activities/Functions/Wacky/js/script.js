0741f1eb3df86e499da42db3f74e694c4a8f6d5f
John Mitchell <JDMitchell@fullsail.edu>
2014-01-28 14:05:35 -0500

    completely changed parts of code to try and make it work. stuck. won’t print to console.

--- Assignments and Activities/Functions/Wacky/js/script.js
+++ Assignments and Activities/Functions/Wacky/js/script.js
@@ -4,9 +4,17 @@ Functions - Wacky
 January 28, 2014
 */
 
+var ammountInBank
+var change
+var amountChange
 
 
-function getInfo(){	//function for gather information from user
+getInfo();	//puts the function into actin
+balanceCheckBook(change, ammountInBank, amountChange);	//Puts the function into action
+
+
+
+function getInfo(change){	//function for gather information from user
 	var fname = prompt("Enter your first name.")	//gets first name
 	console.log(fname)	//prints first name
 
@@ -19,18 +27,26 @@ function getInfo(){	//function for gather information from user
 	var ammountInBank = prompt("How much is in the bank account?") //Gets amount in the bank
 	console.log(ammountInBank);	//prints amount in bank
 
-	var change = prompt("Are you adding to your bank account or taking away from?\nUse '+' to add and '-' to subtract.")	//requesting to know the action being done to the account
-	console.log(change)	//prints action being done.
-
 	var amountChange = prompt("How much is being changed?")	//collects the amount thats being added or removed.
 	console.log(amountChange);	//prints amount being changed 
+	return change;
 }
 
-function balanceCheckBook(){	//Does the calculations
-	//insert code
+function balanceCheckBook(ammountInBank, amountChange){	//Does the calculations
+	var change = prompt("Are you adding to your bank account or taking away from?\nUse '+' to add and '-' to subtract.")	//requesting to know the action being done to the account
+	console.log(change)	//prints action being done.
+	var ammountInBank
+	var amountChange
+	function balance(ammountInBank, amountChange){
+		if(change === '-' ){
+			var newAmount = ammountInBank - amountChange;
+			console.log(newAmount);
+		}else if(change === '+'){
+			var newAmount = ammountInBank + amountChange;
+			console.log(newAmount)
+		}else{
+			console.log("Make sure you use '+' or '-'.")
+		}
+	}
 }
 
-
-
-getInfo();	//puts the function into actin
-balanceCheckBook();	//Puts the function into action
