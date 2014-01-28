930f7699461f9313e4ca80927aabd83ed2718bd4
John Mitchell <JDMitchell@fullsail.edu>
2014-01-28 13:16:57 -0500

    created header and function layout added code to gather info from user.

--- Assignments and Activities/Functions/Wacky/js/script.js
+++ Assignments and Activities/Functions/Wacky/js/script.js
@@ -1,1 +1,27 @@
-alert("This is in my js file");
+/*
+John Mitchell
+Functions - Wacky
+January 28, 2014
+*/
+
+
+
+function getInfo(){	//function for gather information from user
+	var fname = prompt("Enter your first name.")
+	console.log(fname)
+
+	var lname = prompt("Enter your last name.")
+	console.log(lname)
+
+	var fullName = fname + " " + lname
+	console.log(fullName)
+}
+
+function balanceCheckBook(){	//Does the calculations
+	//insert code
+}
+
+
+
+getInfo();	//puts the function into actin
+balanceCheckBook();	//Puts the function into action
