11707f555e15141556b4579f71778e1b6a9a209b
John Mitchell <JDMitchell@fullsail.edu>
2014-01-27 22:39:15 -0500

    added two variables for calculations added the prompt added an if or else if statement for each site and an else statement to correct user if needed created conversion rate for accurate payouts

--- Assignments and Activities/Functions/Personal/js/script.js
+++ Assignments and Activities/Functions/Personal/js/script.js
@@ -9,6 +9,9 @@ January 27, 2008
 
 alert("This will help you calculate your isk needed to pay for your monthly subscription.\nThis assumes you do not want to play your real cash.")
 
+var siteIsk;
+var siteLP;
+
 var getInfo = function(){
 	var plexPricesInGame = prompt("What is the average price of PLEX on the market?")
 	console.log(plexPricesInGame)
@@ -23,7 +26,30 @@ var incursions = function(){
 	console.log(incursion)
 	if(incursion === "y" || incursion === "Y"){
 		var sites = ["empty", "scout", "vanguard", "assualt", "headquarters"]
-		console.log("Which of these do you do? \n" + sites[1] + "," + sites[2] + "," + sites[3] + "," + sites[4])
+		var site = prompt("Which of these do you do? \nChoose 1(scouts)-4(headquarters)\n" + sites[1] + ", " + sites[2] + ", " + sites[3] + " or " + sites[4])
+		if(site === "1"){
+			console.log("These provide 50k isk and 50 LP")
+			siteLP = 50;
+			siteIsk = 50000;
+		}else if(site === "2"){
+			console.log("These provide 15 million Isk and 2k LP.")
+			siteIsk = 15000000
+			siteLP = 2000
+		}else if(site === "3"){
+			console.log("These provide 26 million Isk and 5k LP.")
+			siteIsk = 26000000
+			siteLP = 5000
+		}else if(site === "4"){
+			console.log("These provide 45 million Isk and 10k LP.")
+			siteIsk = 45000000
+			siteLP = 10000
+		}else{
+			console.log("Please choose on of the selected options.\n 1, 2, 3, or 4")
+		}
+		console.log("All sites in high security space pay 70% of the payouts listed.")
+		siteLP = siteLP*0.7
+		siteIsk = siteIsk*0.7
+		console.log("You will recieve " + siteIsk + " Isk and " + siteLP + " LP.")
 	}
 }
 
