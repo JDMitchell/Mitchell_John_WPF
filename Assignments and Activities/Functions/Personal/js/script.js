11cb34a0f9a7140ff37a03dd86e37573d2f4e135
John Mitchell <JDMitchell@fullsail.edu>
2014-01-28 10:34:53 -0500

    declared variables globally so that they can be returned correctly hopefully

--- Assignments and Activities/Functions/Personal/js/script.js
+++ Assignments and Activities/Functions/Personal/js/script.js
@@ -11,10 +11,17 @@ alert("This will help you calculate your isk needed to pay for your monthly subs
 
 var siteIsk;
 var siteLP;
+var plexPricesInGame;
+var yourIsk
+var plexPriceUSD
+var incursion
+
+
 
 var getInfo = function(){
 	var plexPricesInGame = prompt("What is the average price of PLEX on the market?")
 	console.log(plexPricesInGame)
+
 	var plexPriceUSD = prompt("How much does PLEX cost in USD currency?")
 	console.log(plexPriceUSD)
 	var yourIsk = prompt("How much Isk do you have to spend?")
