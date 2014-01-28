774fed8031aa76a1a47dac3a218092ba46c0dd57
John Mitchell <JDMitchell@fullsail.edu>
2014-01-27 19:28:14 -0500

    added console output to check values being inputed. created an if statement later. created variable for avg amount of computers per day.

--- Assignments and Activities/Functions/Industry/js/script.js
+++ Assignments and Activities/Functions/Industry/js/script.js
@@ -6,5 +6,18 @@ January 27, 2014
 
 
 var computersForTheWeek = prompt("How many computers do you need to work on for the week to meet scedule?")	//Gathers quota for the week
-var daysWorking = prompt("How many days are you working this week?")	//Request how many days he is scheduled this week
+var daysWorking = prompt("How many days are you working this week?\nEnter 1-7.")	//Request how many days he is scheduled this week
 var lastDay = prompt("Is today your last day of work for the week?(Y/N)")	//Checks to see if today is his last day of the work week
+
+console.log(computersForTheWeek)
+console.log(daysWorking)
+console.log(lastDay)
+
+var dailyAVG = computersForTheWeek / daysWorking
+console.log("You need to do " + dailyAVG + " computers a day.")
+
+if(lastDay === 'Y' || lastDay === 'y'){
+	console.log("Get to work!")
+}else{
+	console.log("You still have time.")
+}
