b8714965aae228e278d964923960d24dffaa8616
John Mitchell <JDMitchell@fullsail.edu>
2014-01-27 21:11:10 -0500

    Created header and initial functions takes user input and prints to console.

--- Assignments and Activities/Functions/Personal/js/script.js
+++ Assignments and Activities/Functions/Personal/js/script.js
@@ -1,1 +1,23 @@
-alert("This is in my js file");
+/*
+John Mitchell
+Functions - Personal
+January 27, 2008
+*/
+
+alert("This will help you calculate your isk needed to pay for your monthly subscription.\nThis assumes you do not want to play your real cash.")
+
+var getInfo = function(){
+	var plexPricesInGame = prompt("What is the average price of PLEX on the market?")
+	console.log(plexPricesInGame)
+	var plexPriceUSD = prompt("How much does PLEX cost in USD currency?")
+	console.log(plexPriceUSD)
+	var yourIsk = prompt("How much Isk do you have to spend?")
+	console.log(yourIsk)
+}
+
+//var plexCalculations = function(){
+	//
+//}
+
+getInfo();
+//plexCalculations();
