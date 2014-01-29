59d8899b67411b8992eef069065555c21b6be194
John Mitchell <JDMitchell@fullsail.edu>
2014-01-28 23:54:31 -0500

    Added comments to code added and interest function

--- Assignments and Activities/Functions/Wacky/js/script.js
+++ Assignments and Activities/Functions/Wacky/js/script.js
@@ -12,7 +12,7 @@ var newAmount
 
 getInfo();	//puts the function into actin
 balanceCheckBook(ammountInBank, amountChange);	//Puts the function into action
-
+bonus(newAmount)
 
 
 function getInfo(){	//function for gather information from user
@@ -27,29 +27,37 @@ function getInfo(){	//function for gather information from user
 
 	ammountInBank = prompt("How much is in the bank account?") //Gets amount in the bank
 	console.log(ammountInBank);	//prints amount in bank
-	Number(ammountInBank)
+	Number(ammountInBank)	//converts to number
 
 	amountChange = prompt("How much is being changed?")	//collects the amount thats being added or removed.
 	console.log(amountChange);	//prints amount being changed 
-	Number(amountChange)	
+	Number(amountChange)	//converts to number
 }
 
 function balanceCheckBook(ammountInBank, amountChange){	//Does the calculations
 	var change = prompt("Are you adding to your bank account or taking away from?\nUse '+' to add and '-' to subtract.")	//requesting to know the action being done to the account
 	console.log(change)	//prints action being done.
-	balance(change, Number(ammountInBank), Number(amountChange));
+	balance(change, Number(ammountInBank), Number(amountChange));	//function call
 
 }
 
-function balance(change, ammountInBank, amountChange){
-	if(change === '-' ){
-		newAmount = ammountInBank - amountChange;
-		Number(newAmount)
-		console.log(newAmount)
-	}else if(change === '+'){
-		newAmount = ammountInBank + amountChange;
-		console.log(newAmount)
-	}else{
-		console.log("Make sure you use '+' or '-'.")
+function balance(change, ammountInBank, amountChange){	//Function creation
+	if(change === '-' ){	//If decision
+		newAmount = ammountInBank - amountChange;	//calculates new balance
+		Number(newAmount)	//Converts to Number
+		console.log(newAmount)	//Prints to console
+	}else if(change === '+'){	//If decision
+		newAmount = ammountInBank + amountChange;	//calculates new balance
+		Number(newAmount)	//converts to number
+		console.log(newAmount)	//prints to console
+	}else{	//user error statement
+		console.log("Make sure you use '+' or '-'.")	//Corrective statement for the user
 	}
+}
+
+function bonus(newAmount){	//funciton creation
+	var interest = newAmount * 0.025	//calculates interest
+	var savingsBonus = newAmount * 0.025 + newAmount	//calculates saving bonus
+	console.log(interest)	//prints to console
+	console.log(savingsBonus)	//prints saving bonus
 }
