a1064aad4a323207a523d15a902cc6b95a37728c
John Mitchell <JDMitchell@fullsail.edu>
2014-01-28 23:25:26 -0500

    Asked guidance from instructor. removed second variable declarations removed return for change in getInfo

--- Assignments and Activities/Functions/Wacky/js/script.js
+++ Assignments and Activities/Functions/Wacky/js/script.js
@@ -7,6 +7,7 @@ January 28, 2014
 var ammountInBank
 var change
 var amountChange
+var newAmount
 
 
 getInfo();	//puts the function into actin
@@ -14,7 +15,7 @@ balanceCheckBook(ammountInBank, amountChange);	//Puts the function into action
 
 
 
-function getInfo(change){	//function for gather information from user
+function getInfo(){	//function for gather information from user
 	var fname = prompt("Enter your first name.")	//gets first name
 	console.log(fname)	//prints first name
 
@@ -24,26 +25,29 @@ function getInfo(change){	//function for gather information from user
 	var fullName = fname + " " + lname //creates full name
 	console.log(fullName)	//prints fullname
 
-	var ammountInBank = prompt("How much is in the bank account?") //Gets amount in the bank
+	ammountInBank = prompt("How much is in the bank account?") //Gets amount in the bank
 	console.log(ammountInBank);	//prints amount in bank
+	Number(ammountInBank)
 
-	var amountChange = prompt("How much is being changed?")	//collects the amount thats being added or removed.
+	amountChange = prompt("How much is being changed?")	//collects the amount thats being added or removed.
 	console.log(amountChange);	//prints amount being changed 
-	return change;
+	Number(amountChange)	
 }
 
 function balanceCheckBook(ammountInBank, amountChange){	//Does the calculations
 	var change = prompt("Are you adding to your bank account or taking away from?\nUse '+' to add and '-' to subtract.")	//requesting to know the action being done to the account
 	console.log(change)	//prints action being done.
-	balance(change, ammountInBank, amountChange);
+	balance(change, Number(ammountInBank), Number(amountChange));
+
 }
 
 function balance(change, ammountInBank, amountChange){
 	if(change === '-' ){
-		var newAmount = ammountInBank - amountChange;
-		console.log(newAmount);
+		newAmount = ammountInBank - amountChange;
+		Number(newAmount)
+		console.log(newAmount)
 	}else if(change === '+'){
-		var newAmount = ammountInBank + amountChange;
+		newAmount = ammountInBank + amountChange;
 		console.log(newAmount)
 	}else{
 		console.log("Make sure you use '+' or '-'.")
