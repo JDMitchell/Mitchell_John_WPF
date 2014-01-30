6da456b9879b1e1df4abfdba944c8c02394aba5f
John Mitchell <JDMitchell@fullsail.edu>
2014-01-30 12:50:18 -0500

    created file structure for while section of screencasts fallowed along with first video

---
+++ Screencasts and Videos/006 Loops/js/script.js
@@ -0,0 +1,8 @@
+console.log("-------- Loops ---------");
+
+var b = 10	//setups up index
+
+while (b > 0){	//ckecks the condition
+	console.log(b + " kegs on the wall.")
+	b --;	//increments or decrements the index.
+}
