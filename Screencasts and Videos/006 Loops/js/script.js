44857781e8ceed1d6fef443dd2b1a490a51d16d7
John Mitchell <JDMitchell@fullsail.edu>
2014-01-31 10:52:46 -0500

    Followed along with the for loop and do while loop video

--- Screencasts and Videos/006 Loops/js/script.js
+++ Screencasts and Videos/006 Loops/js/script.js
@@ -5,4 +5,22 @@ var b = 10	//setups up index
 while (b > 0){	//ckecks the condition
 	console.log(b + " kegs on the wall.")
 	b --;	//increments or decrements the index.
+
+
+}
+
+console.log("------- Do While Loop ----------")
+
+var c = 10
+
+do{
+	console.log(c + " Kegs on the wall")
+	c --;
+}
+while(c > 0)
+
+console.log("------------For Loops----------")
+
+for (var i = 10; i > 0; i--){
+	console.log(i + " kegs on the wall.")
 }
